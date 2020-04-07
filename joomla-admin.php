ea6274ea72fec45644363e11cb2b18b219f69ef9
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2020-04-07 10:23:43 +0200

    Better reporting

--- joomla-admin.php
+++ joomla-admin.php
@@ -7,12 +7,13 @@ extract(prepare_options(getopt('', [
   'dir:',
   'config:',
   'report',
+  'users',
 ]), [
   'dir' => '.',
   'config' => 'configuration.php',
 ]));
 
-if (isset($help)) {
+if ($argc == 1 || isset($help)) {
   $me = basename(__FILE__);
   echo "\n\tphp $me [OPTIONS]
 
@@ -21,25 +22,47 @@ if (isset($help)) {
     --help      This screen.
     --dir       Path to Joomla directory
     --config    Name of config file ('configuration.php' by default)
-    --report    Dump of all constatns and selected variables extracted from config file
+    --report    Dump of all constants and selected variables extracted from config file
                 plus some selected options extracted from database
+    --users
   ";
   exit;
 }
 
 $dir = rtrim($dir, '/');
-$config_file = realpath("$dir/$config");
 
-if ($config_file === false && file_exists($config_file) === false) {
-  info("Config file $config_file does not exist");
-  exit;
+define('_JEXEC', true);
+define('JPATH_BASE', $dir);
+$defines_php = JPATH_BASE . '/includes/defines.php';
+$framework_php = JPATH_BASE . '/includes/framework.php';
+
+if (file_exists($defines_php)) {
+  require_once $defines_php;
+
+  if (file_exists($framework_php)) {
+    require_once $framework_php;
+
+  } else {
+    info("File $framework_php does not exist");
+  }
+} else {
+  info("File $defines_php does not exist");
 }
 
-include "$config_file";
-
-$settings = get_class_vars('JConfig');
 
 if (isset($report)) {
+  if (false === class_exists('JConfig')) {
+    $config_file = realpath("$dir/$config");
+
+    if ($config_file === false && file_exists($config_file) === false) {
+      failure("File $config_file does not exist");
+    }
+
+    include "$config_file";
+  }
+
+  $settings = get_class_vars('JConfig');
+
   foreach ($settings as $setting => $value) {
     if ($value === false) $value = 'false';
     if ($value === null) $value = 'NULL';
@@ -49,4 +72,21 @@ if (isset($report)) {
       info("$setting = $value");
     }
   }
+
+  $constants = get_defined_constants(true);
+
+  foreach ($constants['user'] as $constant => $value) {
+    if (in_array($constant, ['EXIT_SUCCESS', 'EXIT_FAILURE'])) continue;
+    if ($value === false) $value = 'false';
+    if ($value === null) $value = 'NULL';
+    if ($value === '') {
+      info("$constant is an empty string");
+    } else {
+      info("$constant = $value");
+    }
+  }
+}
+
+if (isset($users)) {
+
 }
