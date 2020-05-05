ee523dd5c079970aac07ab44ddcc39fe7850f648
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2020-05-05 09:41:57 +0200

    Dump user databases

--- mysql-admin.php
+++ mysql-admin.php
@@ -15,6 +15,7 @@ extract(prepare_options(getopt('', [
   'mysql:',
   'mysqladmin:',
   'mysqldump:',
+  'dump:',
 ]), [
   'config' => 'my.cnf',
   'user' => 'root',
@@ -33,17 +34,18 @@ if ($argc == 1 || isset($help)) {
 
   Possible options:
 
-    --help        This screen.
-    --php         Report on PHP mysql connectivity capabilities
-    --client      Choose one: mysqli [default], PDO, cli
-      --mysql         Path to mysql command
-      --mysqladmin    Path to mysqladmin command
-      --mysqldump     Path to mysqldump command
+    --help          This screen.
+    --php           Report on PHP mysql connectivity capabilities
+    --client=       Choose one: mysqli [default], PDO, cli
+      --mysql=      Path to mysql command
+      --mysqladmin= Path to mysqladmin command
+      --mysqldump=  Path to mysqldump command
     --connect
-      --user
-      --password
-      --host
-      --port
+      --user=
+      --password=
+      --host=
+      --port=
+    --dump=         Dump single database or 'all-databases' or 'user-databases'
   ";
   exit;
 }
@@ -193,4 +195,41 @@ if (isset($connect)) {
     info("Unknown client $client");
   }
 
+  if (isset($dump)) {
+    $cmds = [];
+
+    if ($dump == 'all-databases') {
+      $database = '--all-databases';
+    } else if ($dump == 'user-databases') {
+      $database = '--ignore-database=mysql --ignore-database=performance_schema --ignore-database=information_schema';
+    } else {
+      $database = $dump;
+    }
+
+    $filename = "$host-$dump-". date('Y-m-d-') . time();
+    $file_sql = "$filename.sql";
+    $file_log = "$filename.log";
+    $file_gz  = "$file_sql.sql";
+
+    $cmds[] = "$mysqldump --host=$host --user=$user --password=$password --log-error=$db_log --single-transaction --extended-insert --verbose $database > $file_sql";
+
+    if (posix_getuid() == 0){
+      $cmds[] = 'pv --version > /dev/null || dnf install pv -y || yum install pv -y';
+    }
+    $cmds[] = "pv $file_sql | gzip -c -v > $file_gz || gzip -v $file_sql";
+
+    execute($cmds);
+
+    if (realpath($file_gz)) {
+      info("Database backup complete: " . realpath($file_gz));
+      info("Log file created: " . realpath($file_log));
+    } else {
+      if (realpath($file_sql)) {
+        info("Database backup complete: " . realpath($file_sql));
+        info("Log file created: " . realpath($file_log));
+      } else {
+        failure("Database backup failure");
+      }
+    }
+  }
 }
