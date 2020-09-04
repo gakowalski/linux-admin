1675d0c87c388b913168bc4a57983261c4167272
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2020-09-04 17:27:02 +0200

    Updated external tools

--- external-tools/install.sh
+++ external-tools/install.sh
@@ -1,3 +1,5 @@
+#/bin/bash
 git submodule update --init
 git submodule update --remote --merge
-composer install
+composer install || /usr/local/bin/composer install || php composer.phar install || echo "Composer not installed!"
+composer update || /usr/local/bin/composer update || php composer.phar update || echo "Composer not installed!"
