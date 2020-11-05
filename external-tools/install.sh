ccc7b72b7510eea2bd85688a284f626d26456609
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2020-11-05 14:28:26 +0100

    php-fpmpal as pipelined script instead of git submodule

--- external-tools/install.sh
+++ external-tools/install.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 GIT_TRACE=1 git submodule update --init
-GIT_TRACE=1 git submodule update --remote --merge
+#GIT_TRACE=1 git submodule update --remote --merge
+git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
 composer install || /usr/local/bin/composer install || php composer.phar install || echo "Composer not installed!"
 composer update || /usr/local/bin/composer update || php composer.phar update || echo "Composer not installed!"
---
+++ external-tools/php-fpmpal.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+curl -sL https://raw.githubusercontent.com/haxcop/php-fpmpal/master/php-fpmpal.01.sh | bash
